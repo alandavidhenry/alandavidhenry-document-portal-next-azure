7a6e960992547f605cdd0cb143b29f9d7a91d7dd
Alan Henry <alandavidhenry@gmail.com>
2024-12-07 16:48:08 +0000

    add security headers

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,26 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   env: {
     AZURE_STORAGE_CONTAINER_NAME: process.env.AZURE_STORAGE_CONTAINER_NAME!
-  }
+  },
+  headers: async () => [
+    {
+      source: '/:path*',
+      headers: [
+        {
+          key: 'Strict-Transport-Security',
+          value: 'max-age=31536000; includeSubDomains'
+        },
+        {
+          key: 'X-Frame-Options',
+          value: 'DENY'
+        },
+        {
+          key: 'X-Content-Type-Options',
+          value: 'nosniff'
+        }
+      ]
+    }
+  ]
 }
 
 export default nextConfig
