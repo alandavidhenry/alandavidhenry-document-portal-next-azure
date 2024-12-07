02590e80a05b14cdee38d0fa3589922338f446c8
Alan Henry <alandavidhenry@gmail.com>
2024-12-07 15:56:57 +0000

    add example env

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  env: {
+    AZURE_STORAGE_CONTAINER_NAME: process.env.AZURE_STORAGE_CONTAINER_NAME!
+  }
+}
 
-export default nextConfig;
+export default nextConfig
